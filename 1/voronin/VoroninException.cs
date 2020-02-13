4e63a49bf9455e3c0f35e810d905874f3d5a04d1
Swagrosemedusa <voronin.voronin-maikl@yandex.ru>
2020-02-13 15:55:01 +0300

    laba 3

---
+++ 1/voronin/VoroninException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace voronin
+{
+    class VoroninException : Exception
+    {
+        public VoroninException(string message)
+            : base(message)
+        {
+        }
+    }
+}
