12d6a1378e77b497e3962d5a14b41d8d6f8a5a16
Swagrosemedusa <voronin.voronin-maikl@yandex.ru>
2020-02-13 16:03:02 +0300

    laba 4

--- 1/Program.cs
+++ 1/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using voronin;
 
 namespace _1
 {
@@ -6,7 +9,38 @@ namespace _1
     {
         static void Main(string[] args)
         {
+            string path = @"..\..\..\version";
+            Directory.CreateDirectory("Log");
+            using (StreamReader sr = new StreamReader(path))
+            {
 
+                MyLog.log("Версия_программы: " + sr.ReadToEnd().Trim('\n'));
+            }
+            double a = 0, b = 0, c = 0;
+            Console.WriteLine("Введите 3 параметра a, b и с");
+            a = Convert.ToDouble(Console.ReadLine());
+            b = Convert.ToDouble(Console.ReadLine());
+            c = Convert.ToDouble(Console.ReadLine());
+            MyLog.log("Введено уравнение: " + a + "X^2+" + b + "X+" + c + "=0");
+            try
+            {
+                QuaEquation calc = new QuaEquation();
+                List<double> res = calc.solve(a, b, c);
+                String s = "Корни уравнения: ";
+                for (int i = 0; i < res.Count; i++)
+                {
+                    s += res[i] + " ";
+                }
+
+                MyLog.log(s);
+            }
+            catch (VoroninException e)
+            {
+                MyLog.log("Ошибка: " + e.Message);
+            }
+            MyLog.write();
+            Console.ReadKey();
         }
     }
 }
+
